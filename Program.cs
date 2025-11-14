addfb9c2b5155d08d47fc8903ccaec1ddff2a5ee
FemmeCode <shabalala.landiwe@gmail.com>
2025-11-14 15:49:08 +0200

    removed the openApi extension conflicting with swagger

--- Program.cs
+++ Program.cs
@@ -1,15 +1,26 @@
+//  configuration action passed to the AddSwaggerGen method adds information such as the author, license, and description.
+using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
+
+//  Swagger generator 
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // app.MapOpenApi(); - for minimal Open API extension
+    // Enable the middleware for serving the generated JSON document and the Swagger UI
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
